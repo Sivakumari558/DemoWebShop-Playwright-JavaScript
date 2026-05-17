1ad4cf99ef8d9b1eaa08ead39489696506fbada1
Sivakumari <yarramsivakumari58@gmail.com>
2026-05-17 23:33:27 +0530

    initial framework commit

--- tests/flipkart.spec.js
+++
@@ -1,6 +0,0 @@
-const {test, expect} = require('@playwright/test');
-test('Flipkart Dashboard', async({page}) => {
- await page.goto('https://www.flipkart.com/');
- //await expect(page).toBeVisible(/dashboard/);
- await expect(page).toHaveURL('https://www.flipkart.com/');
-});
